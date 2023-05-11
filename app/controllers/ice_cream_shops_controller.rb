be073f983a2abe2cae7cd7353a571bd7829ac6b2
Andy Weissman <andyweissman6@gmail.com>
2023-05-11 17:42:45 -0600

    singularized @ice_cream_shops (ice_cream_shop) bc only need one instance of, not all

--- app/controllers/ice_cream_shops_controller.rb
+++ app/controllers/ice_cream_shops_controller.rb
@@ -2,4 +2,10 @@ class IceCreamShopsController < ApplicationController
   def index
     @ice_cream_shops = IceCreamShop.all
   end
+
+  def show
+    @ice_cream_shop = IceCreamShop.find(params[:id])
+    # require 'pry'; binding.pry
+    # just looking for one instance, not all. so not pluralizing
+  end
 end
