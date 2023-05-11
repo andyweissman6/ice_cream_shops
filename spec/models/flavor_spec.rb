dd76ee50abd752b674a65675ac548f2afdab86b7
Andy Weissman <andyweissman6@gmail.com>
2023-05-10 21:08:46 -0600

    test relationship Flavor to Ice cream shop

--- spec/models/flavor_spec.rb
+++ spec/models/flavor_spec.rb
@@ -1,7 +1,8 @@
-require "rails_helper"
+require 'rails_helper'
+# require_relative '../../app/models/ice_cream_shop.rb'
 
-RSpec.describe Flavor, type :model do
-  describe "relationships" do
+RSpec.describe Flavor, type: :model do
+  describe 'relationships' do
     it { should belong_to :ice_cream_shop }
   end
 end
