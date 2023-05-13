9926b33d55f87198c4aed9ed82c81be0289c66cb
Andy Weissman <andyweissman6@gmail.com>
2023-05-12 18:02:56 -0600

    format change: add space in let statement

--- spec/features/ice_cream_shops/index_spec.rb
+++ spec/features/ice_cream_shops/index_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe "/ice_cream_shops (IceCreamShop Index page)", type: :feature do
   #User Story 1
   describe "as a visitor, when I visit IceCreamShop index page" do
-    let!(:sprinkles) {IceCreamShop.create!(name: "Sprinkles",
+    let!(:sprinkles) { IceCreamShop.create!(name: "Sprinkles",
                                         open_24_hrs: true,
                                         num_employees: 6) }
 
-    let!(:nugs) {IceCreamShop.create!(name: "Nugs", 
+    let!(:nugs) { IceCreamShop.create!(name: "Nugs", 
                                     open_24_hrs: false,
                                     num_employees: 3) }
 
