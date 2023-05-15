fd2527f65d74e9343050f3a991953caf84ef002e
Andy Weissman <andyweissman6@gmail.com>
2023-05-14 19:33:20 -0600

    reformated spacing

--- spec/features/ice_cream_shops/flavors/index_spec.rb
+++ spec/features/ice_cream_shops/flavors/index_spec.rb
@@ -2,18 +2,20 @@ require "rails_helper"
 
 RSpec.describe "/parents/:parent_id/child_table_name", type: :feature do
   describe "As a visitor, when I Icecream Shop Flavors index page" do
-    let!(:sprinkles) { IceCreamShop.create!(name: "Sprinkles",
-                                        open_24_hrs: true,
-                                        num_employees: 6) }
+
+    let!(:sprinkles) {IceCreamShop.create!(   name: "Sprinkles",
+                                              open_24_hrs: true,
+                                              num_employees: 6) }
+                                        
 
     let!(:vanilla) {sprinkles.flavors.create!(  flavor_name: "vanilla",
-                                      vegan: true,
-                                      grams_sugar: 50 )}
+                                                vegan: true,
+                                                grams_sugar: 50 )}
     
     let!(:snozzberry) {sprinkles.flavors.create!( flavor_name: "snozzberry",
-                                        vegan: false,
-                                        grams_sugar: 69 )}
-
+                                                  vegan: false,
+                                                  grams_sugar: 69 )}
+    
     it "I see each flavor that is associated with that icecream shop with each flavor's attributes" do
       visit "/ice_cream_shops/#{sprinkles.id}/flavors"
 
