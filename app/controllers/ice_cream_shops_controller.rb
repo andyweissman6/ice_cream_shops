99dd95e9cd98089a07b9fcf0c4e0c8eecb49a6d6
Andy Weissman <andyweissman6@gmail.com>
2023-05-15 19:07:53 -0600

    create 'new' action and 'create' action, then redirect after creation to ice cream shops index

--- app/controllers/ice_cream_shops_controller.rb
+++ app/controllers/ice_cream_shops_controller.rb
@@ -8,4 +8,16 @@ class IceCreamShopsController < ApplicationController
     # require 'pry'; binding.pry
     # just looking for one instance, not all. so not pluralizing
   end
+
+  def new
+  end
+
+  def create
+    IceCreamShop.create(name: params[:name], 
+                        open_24_hrs: params[:open_24_hrs],
+                        num_employees: params[:num_employees])
+
+    redirect_to "/ice_cream_shops"
+    # require 'pry'; binding.pry
+  end
 end
