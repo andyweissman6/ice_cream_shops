e38d2edf1649c31fddc4897ae9e87365e00f9f5c
Andy Weissman <andyweissman6@gmail.com>
2023-05-16 16:33:00 -0600

    add get/#new, post/#create, get/#edit, patch/#update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 # 5 VERBS = get, post, patch/put, delete
 
-  get "/ice_cream_shops/new", to: "ice_cream_shops#new"
-  post "/ice_cream_shops/new", to: "ice_cream_shops#create"
   get "/ice_cream_shops", to: "ice_cream_shops#index"
+  get "/ice_cream_shops/new", to: "ice_cream_shops#new"
+  post "/ice_cream_shops", to: "ice_cream_shops#create"
   get "/ice_cream_shops/:id", to: "ice_cream_shops#show"
+  get "/ice_cream_shops/:id/edit", to: "ice_cream_shops#edit"
+  patch "/ice_cream_shops/:id", to: "ice_cream_shops#update"
   get "/flavors", to: "flavors#index"
   get "/flavors/:id", to: "flavors#show"
   get "/ice_cream_shops/:id/flavors", to: "ice_cream_shops/flavors#index"
