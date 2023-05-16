3b7c16c2ba94022c416f99ac59dbe6d94c8feef0
Andy Weissman <andyweissman6@gmail.com>
2023-05-15 19:10:11 -0600

    add get /ice_cream_shops/new and post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # 5 VERBS = get, post, patch/put, delete
-  # Defines the root path route ("/")
-  # root "articles#index"
+# 5 VERBS = get, post, patch/put, delete
+
+  get "/ice_cream_shops/new", to: "ice_cream_shops#new"
+  post "/ice_cream_shops/new", to: "ice_cream_shops#create"
   get "/ice_cream_shops", to: "ice_cream_shops#index"
   get "/ice_cream_shops/:id", to: "ice_cream_shops#show"
   get "/flavors", to: "flavors#index"
   get "/flavors/:id", to: "flavors#show"
   get "/ice_cream_shops/:id/flavors", to: "ice_cream_shops/flavors#index"
   get "/", to: "flavors#index"
-  # can also write it like:
-  # "/ice_cream_shops/:ice_cream_shop_id/flavors", to: "ice_cream_shops/flavors#index"
-  # in pry --> params: will see :ice_cream_shop_id = number
 
 end
